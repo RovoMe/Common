9d88db99fec53002b5f8c2744d8f2a8b696c5cd8
Roman Vottner <rovo@gmx.at>
2014-07-23 12:00:41 +0200

    ADDED possibility to include line break symbols to the end of each line contained in the HTML document

--- src/main/java/at/rovo/common/UrlReader.java
+++ src/main/java/at/rovo/common/UrlReader.java
@@ -11,50 +11,58 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * <p>
  * <em>UrlReader</em> reads a HTML pages based on the URL of this page provided
  * as string and loads the content into a BufferedReader if
  * {@link #read(String)} is invoked or directly in a string containing all the
  * page's content using {@link #readPage(String)}.
- * </p>
- * <p>
+ * <p/>
  * Some pages require cookies to be set to present their content or even
  * redirect to further pages. <em>UrlReader</em> is capable of reading those
  * pages too.
- * </p>
  * 
  * @author Roman Vottner
  */
 public class UrlReader 
 {
-	/** The logger of this class **/
-	private static Logger logger = LogManager.getLogger(UrlReader.class.getName());
-	/** The cookie received from a visited page **/
+	/** The logger of this class */
+	private static Logger LOG = LogManager.getLogger(UrlReader.class.getName());
+	/** The cookie received from a visited page */
 	private String cookie = null;
-	/** The original URL of a page **/
+	/** The original URL of a page */
 	private String originURL = null;
-	/** The URL after a redirect **/
+	/** The URL after a redirect */
 	private String realURL = null;
-	
+	/** Specifies if the output should contain line-breaks */
+	private boolean includeLineBreaks;
+
 	/**
-	 * <p>
 	 * Creates a new instance of this class.
-	 * </p>
 	 */
 	public UrlReader()
 	{
-		
+		includeLineBreaks = false;
+	}
+
+	/**
+	 * Creates a new instance of this class and specifies to either include
+	 * line breaks in the output if <em>includeLineBreaks</em> is set to true
+	 * or to omit them if this argument is set to false.
+	 *
+	 * @param includeLineBreaks
+	 *            If set to true specifies to include line breaks within the
+	 *            output, otherwise line breaks will be omitted
+	 */
+	public UrlReader(boolean includeLineBreaks)
+	{
+		this.includeLineBreaks = includeLineBreaks;
 	}
 	
 	/**
-	 * <p>
 	 * Reads the content of a web document by downloading the content of the
 	 * specified page.
-	 * </p>
-	 * <p>
+	 * <p/>
 	 * If any redirects are necessary, this method will follow these to read the
 	 * content of the page.
-	 * </p>
 	 * 
 	 * @param url
 	 *            The URL of the HTML page to load
@@ -73,7 +81,7 @@ public class UrlReader
 		// used an approach presented by tim_yates at stackoverflow.com
 		// (http://stackoverflow.com/questions/7055957/httpurlconnection-to-get-title-of-the-content-and-got-moved-permanently)
 		// as pages from nytimes require cookies
-		HttpURLConnection httpConn = null;
+		HttpURLConnection httpConn;
 		InputStreamReader reader = null;
 		this.originURL = url;
 		this.realURL = url;
@@ -96,22 +104,24 @@ public class UrlReader
 			
 			// Try and get a cookie the site will set, we will pass this next time round
 			cookie = httpConn.getHeaderField("Set-Cookie");
-			
+
+			// TODO: respect pages encoding-settings
 			reader = new InputStreamReader(httpConn.getInputStream(), StandardCharsets.UTF_8);
 //			reader = new InputStreamReader(new URL(url).openStream(), "8859_1");
+		}
+		if (reader == null)
+		{
+			throw new IOException("Could not read input source");
 		}
 		return new BufferedReader(reader);
 	}
 	
 	/**
-	 * <p>
 	 * Reads the content of a web document by downloading the content of the
 	 * specified page.
-	 * </p>
-	 * <p>
+	 * <p/>
 	 * If any redirects are necessary, this method will follow these to read the
 	 * content of the page.
-	 * </p>
 	 * 
 	 * @param url
 	 *            The URL of the HTML page to load
@@ -124,29 +134,36 @@ public class UrlReader
 	{
 		this.checkURL(url);
 		
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder buffer = new StringBuilder();
 		try
 		{
 			BufferedReader reader = read(url);
 			String line = reader.readLine();
-			String curHTML = "";
 			while (line != null)
 			{
-				curHTML = buffer.toString();
+				String curHTML = buffer.toString();
 				if (curHTML.endsWith(" ") && !line.startsWith(" "))
 					buffer.append(line);
 				else if (curHTML.endsWith(" ") && line.startsWith(" "))
 					buffer.append(line.trim());
 				else if (!curHTML.endsWith(" ") && line.startsWith(" "))
 					buffer.append(line);
-				else
-					buffer.append(" "+line);
+				else if (!line.trim().equals(""))
+				{
+					buffer.append(" ");
+					buffer.append(line);
+				}
+				// add a line break if requested
+				if (this.includeLineBreaks)
+				{
+					buffer.append("\n");
+				}
 				line = reader.readLine();
 			}
 		}
 		catch(IOException ioEx)
 		{
-			logger.warn("Could not read {}! Reason: {}", url, ioEx.getLocalizedMessage());
+			LOG.warn("Could not read {}! Reason: {}", url, ioEx.getLocalizedMessage());
 			return null;
 		}
 		return buffer.toString();
@@ -159,9 +176,7 @@ public class UrlReader
 	}
 	
 	/**
-	 * <p>
 	 * Returns the original URL this instance was requested to load a page from.
-	 * </p>
 	 * 
 	 * @return The original URL as provided by a caller
 	 */
@@ -181,14 +196,11 @@ public class UrlReader
 	}
 	
 	/**
-	 * <p>
 	 * An entrance point to downloading pages which uses a URL specified as
 	 * parameter and prints its content to the specified log4j2 log file.
-	 * </p>
-	 * <p>
+	 * <p/>
 	 * The page to download should be provided as the first argument. Any other
 	 * arguments will be ignored.
-	 * </p>
 	 * 
 	 * @param args
 	 *            The arguments passed to UrlReader - only the first is used
@@ -202,12 +214,12 @@ public class UrlReader
 		UrlReader reader = new UrlReader();
 		String url = args[0];
 		BufferedReader br = reader.read(url);
-		logger.debug("Reading origin url: {}", reader.getOriginURL());
-		logger.debug("Reading real url: {}", reader.getRealURL());
+		LOG.debug("Reading origin url: {}", reader.getOriginURL());
+		LOG.debug("Reading real url: {}", reader.getRealURL());
 		String line = br.readLine();
 		while (line != null)
 		{
-			logger.debug(line);
+			LOG.debug(line);
 			line = br.readLine();
 		}
 	}
